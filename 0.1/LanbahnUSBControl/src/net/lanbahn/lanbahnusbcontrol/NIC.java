734b5d55f53ba15aadcd8b2e95f56cfd9aa38989
Michael Blank <michael@blank-edv.de>
2016-04-18 10:26:33 +0200

    added lanbahn.net

--- 0.1/LanbahnUSBControl/src/net/lanbahn/lanbahnusbcontrol/NIC.java
+++ 0.1/LanbahnUSBControl/src/net/lanbahn/lanbahnusbcontrol/NIC.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright:  Michael Blank
+  *  Copyright:  Michael Blank - 2016 - lanbahn.net
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
